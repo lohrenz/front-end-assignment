6175bc3a194f77102f8d9c3b1036038af3425ba2
L. Amatkasan <L.r.k.amatkasan@umail.leidenuniv.nl>
2024-09-26 22:06:52 +0200

    base css done, api get notes working

--- frontend/src/components/NoteItem/NoteItems.js
+++ frontend/src/components/NoteItem/NoteItems.js
@@ -1,10 +1,28 @@
-import React from 'react'
+import React from 'react';
+import styles from './NoteItem.module.css';
+import { ReactComponent as DelIcon } from '../../assets/basic_trashcan.svg';
+import { ReactComponent as UpdateIcon } from '../../assets/software_pencil.svg';
 
-function NoteItems() {
+
+const getTime = (note) => {
+    return new Date(note.updated).toLocaleDateString();
+}
+
+function NoteItems({ note }) {
     return (
-        <div>
-            <h1>NOTE</h1>
-        </div>
+        <div className={styles.noteItem}>
+            <h2 className={styles.noteTitle}>{note.title}</h2>
+            <p className={styles.noteText}>{note.body}</p>
+            <div className={styles.noteAction}>
+                <button className={styles.noteBtn}>
+                    <DelIcon />
+                </button>
+                <button className={styles.noteBtn}>
+                    <UpdateIcon />
+                </button>
+            </div>
+            <p className={styles.itemDate}><span>{getTime(note)}</span></p>
+        </ div >
     )
 }
 
--- frontend/src/components/Notepage/NotePageList.js
+++ frontend/src/components/Notepage/NotePageList.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './NotePageList.module.css';
 import NoteItem from '../NoteItem/NoteItems';
-
+import { ReactComponent as AddIcon } from '../../assets/add.svg';
 
 function NotePageList() {
+    const [notes, setNotes] = useState([]);
+    useEffect(() => {
+        getNotes();
+    }, []);
+
+    const getNotes = async () => {
+        try {
+            const response = await fetch('/api/notes/');
+            if (response.ok) {
+                const jsonResponse = await response.json();
+                console.log(jsonResponse);
+                setNotes(jsonResponse);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    console.log(notes);
     return (
         <div className={styles.notepagelist}>
-            <NoteItem />
+            {notes.map((note, index) => (
+                <NoteItem key={index} note={note} />
+            ))}
+            <button className={styles.newNote}>
+                <AddIcon />
+            </button>
         </div >
     )
 }
